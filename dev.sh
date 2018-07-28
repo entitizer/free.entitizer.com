0f03093de6c8c24a4951fd4e4db5fa6344a688dd
Dumitru Cantea <canteadumitru@gmail.com>
2018-07-28 15:37:21 +0300

    fix dev.sh & prod.sh

--- dev.sh
+++ dev.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+yarn remove @textactor/domain
 yarn remove @textactor/ner
 yarn remove @textactor/wikientity-data
+yarn remove @textactor/wikientity-domain
 yarn remove @textactor/actor-data
 
+yarn link @textactor/domain
 yarn link @textactor/ner
 yarn link @textactor/wikientity-data
+yarn link @textactor/wikientity-domain
 yarn link @textactor/actor-data
 
 yarn tsc
--- prod.sh
+++ prod.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+yarn unlink @textactor/domain
 yarn unlink @textactor/ner
 yarn unlink @textactor/wikientity-data
+yarn unlink @textactor/wikientity-domain
 yarn unlink @textactor/actor-data
 
+yarn add @textactor/domain
 yarn add @textactor/ner
 yarn add @textactor/wikientity-data
+yarn add @textactor/wikientity-domain
 yarn add @textactor/actor-data
 
 yarn tsc
