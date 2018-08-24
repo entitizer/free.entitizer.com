81efbe4affc0e083b691dbb5865d41fd41742785
Dumitru Cantea <canteadumitru@gmail.com>
2018-08-24 11:26:23 +0300

    fix log errors

--- src/utils.ts
+++ src/utils.ts
@@ -11,7 +11,7 @@ export function sendSuccess(res: Response, data: any) {
 }
 
 export function sendError(res: Response, status: number, error: any) {
-    logger.error('error', error);
+    logger.error(error);
     const result = {
         error: { message: error.message }
     };
